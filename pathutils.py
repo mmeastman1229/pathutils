0e8160a09f67c8753c717affde2e29ebd995176c
mmeastman1229 <matthew.m.eastman@gmail.com>
2023-10-29 07:36:16 -0700

    Implementation Change
    Chaged the implementation of the helper from a dictionary to a
    namedtupel.

--- pathutils.py
+++ pathutils.py
@@ -25,6 +25,7 @@ When importing the file, alias the the library as "putil"
 """
 
 from collections import Counter
+from collections import namedtuple
 from filecmp import cmp
 from pathlib import Path
 from pprint import pprint
@@ -41,86 +42,193 @@ ERROR_NOT_PATH_DICT = 'ERROR: Not a dictionary. ' \
 SEARCH_PATTERN = '*'
 
 
+PHelper = namedtuple('PHelper', [
+                        'absolute',
+                        'all_dirs',
+                        'all_files',
+                        'anchor',
+                        'as_posix',
+                        'as_uri',
+                        'change_name',
+                        'change_name_and_type',
+                        'change_type',
+                        'chmod',
+                        'cwd',
+                        'dir_contents',
+                        'display_tree',
+                        'drive',
+                        'exists',
+                        'expanduser',
+                        'files',
+                        'glob',
+                        'group',
+                        'hardlink_to',
+                        'home',
+                        'info',
+                        'is_absolute',
+                        'is_block_device',
+                        'is_char_device',
+                        'is_dir',
+                        'is_fifo',
+                        'is_file',
+                        'is_mount',
+                        'is_relative_to',
+                        'is_reserved',
+                        'is_socket',
+                        'is_symlink',
+                        'iterdir',
+                        'joinpath',
+                        'lchmod',
+                        'link_to',
+                        'lstat',
+                        'match',
+                        'mkdir',
+                        'name',
+                        'ndirs',
+                        'nfiles',
+                        'open',
+                        'owner',
+                        'parent',
+                        'parents',
+                        'parts',
+                        'path',
+                        'ptype'
+                        'read_bytes',
+                        'read_text',
+                        'readlink',
+                        'relative_to',
+                        'rename',
+                        'replace',
+                        'resolve',
+                        'rglob',
+                        'rmdir',
+                        'root',
+                        'samefile',
+                        'stat',
+                        'stem',
+                        'subdirs'
+                        'suffix',
+                        'suffixes',
+                        'symlink_to',
+                        'total_lf_cnt',
+                        'total_nf_cnt',
+                        'total_nsdir_cnt'
+                        'total_subdir_cnt',
+                        'touch',
+                        'unlink',
+                        'write_bytes',
+                        'write_text'])
+
+
 def create_path(directory):
     return Path(directory)
 
 
-def create_helper_item_collection(path):
-    
-    lfiles, ldirs = get_local_counts(path)
-    nfiles, ndirs = get_nested_counts(path)
+def create_helper(path):
 
-    properties_and_methods = dict(
-        # Main Path object info
-        path=path
-        , keys=None
-        , name=path.name 
-        , exists=path.exists()
-        , is_dir=path.is_dir()
-        , is_file=path.is_file()
-        , ptype=get_ptype(path)
-        , home=path.home()
-        , cwd=path.cwd()
-        , is_absolute=path.is_absolute()
-        , parents=list(path.parents)
-        , root=path.root
-        , drive=path.drive
-        , anchor=path.anchor # combination of root and drive
-        , stem=path.stem  # name of file without suffix
-        , suffix=path.suffix
-        , suffixes=path.suffixes
-        , parts=path.parts
-        
-        # Files and Folder Info
-        , subdirs=get_subdirs(path)
-        , files=get_files(path)
-        , all_files=get_nested_files(path)
-        , all_dirs=get_nested_subdirs(path)
-        , dir_contents=get_all_contents(path)
-        , total_local_files=lfiles
-        , total_local_dirs=ldirs
-        , total_files=nfiles
-        , total_subdirs=ndirs
-        
-        # Other useful functions
-        , join=path.joinpath
-        , rename_file=path.with_stem
-        , change_type=path.with_suffix
-        , change_name_and_type=path.with_name
-        , read_text=path.read_text
-        , read_bytes=path.read_bytes
-        , display_tree=display_directory_tree
-        , info=show_path_info
-        )
-    
-    properties_and_methods['keys'] = get_keys(properties_and_methods)
-    return properties_and_methods
+    path = PHelper(
+             absolute=path.absolute(),
+             all_dirs=get_nested_subdirs,
+             all_files=get_nested_files,
+             anchor=path.anchor,
+             as_posix=path.as_posix,
+             as_uri=path.as_uri,
+             change_name=path.with_stem,
+             change_name_and_type=path.withname,
+             change_type=path.with_suffix,
+             chmod=path.chmod,
+             cwd=path.cwd(),
+             dir_contents=get_all_contents,
+             display_tree=display_directory_tree,
+             drive=path.drive,
+             exists=path.exists(),
+             expanduser=path.expanduser,
+             files=get_files,
+             glob=path.glob,
+             group=path.group,
+             hardlink_to=path.hardlink_to,
+             home=path.home(),
+             info=show_path_info,
+             is_absolute=path.is_absolute(),
+             is_block_device=path.is_block_device(),
+             is_char_device=path.is_char_device(),
+             is_dir=path.is_dir(),
+             is_fifo=path.is_fifo(),
+             is_file=path.is_file(),
+             is_mount=path.is_mount,
+             is_relative_to=path.is_relative_to,
+             is_reserved=path.is_reserved(),
+             is_socket=path.is_socket(),
+             is_symlink=path.is_symlink(),
+             iterdir=path.iterdir,
+             joinpath=path.joinpath,
+             lchmod=path.lchmod,
+             link_to=path.link_to,
+             lstat=path.lstat,
+             match=path.match,
+             mkdir=path.mkdir,
+             name=path.name,
+             ndirs=get_nested_subdirs,
+             nfiles=get_nested_files,
+             open=path.open,
+             owner=path.owner,
+             parent=path.parent,
+             parents=list(path.parents),
+             parts=path.parts,
+             path=path,
+             ptype=get_ptype(path),
+             read_bytes=path.read_bytes,
+             read_text=path.read_text,
+             readlink=path.readlink,
+             relative_to=path.relative_to,
+             rename=path.rename,
+             replace=path.replace,
+             resolve=path.resolve,
+             rglob=path.rglob,
+             rmdir=path.rmdir,
+             root=path.root,
+             samefile=path.samefile,
+             stat=path.stat,
+             stem=path.stem,
+             subdirs=get_subdirs,
+             suffix=path.suffix,
+             suffixes=path.suffixes,
+             symlink_to=path.symlink_to,
+             total_lf_cnt=get_file_count(path),
+             total_nf_cnt=get_nested_subdir_count(path),
+             total_nsdir_cnt= get_nested_subdir_count(path),
+             total_subdir_cnt=ldirs,
+             touch=path.touch,
+             unlink=path.unlink,
+             write_bytes=path.write_bytes,
+             write_text=path.write_text)
+    return path
 
 
-def get_keys(collection):
-    return [key for key in collection]
+def get_fields(collection):
+    return [field for field in collection._fields]
 
 
-def get_collection_path(path):
-    return path['path']
+def get_collection_path(collection):
+    return collection.path if is_collection(collection)
 
 
 # local file functions
-def get_files(path):
+def get_local_files(path):
     if is_collection(path):
         path = get_collection_path(path)
-    
-    return ([item.name for item in path.iterdir() if item.is_file()])
-    
 
-def get_file_count(path):
+    return ([item.name for item in path.iterdir() if item.is_file()])
+
+
+def get_local_file_count(path):
     if is_collection(path):
         path = get_collection_path(path)
 
     return len([item for item in path.iterdir() if item.is_file()])
 
 
-def list_files(path):
+def list_local_files(path):
     if is_collection(path):
         path = get_collection_path(path)
 
@@ -129,24 +237,24 @@ def list_files(path):
 
 
 # local folders
-def get_subdirs(path):
+def get_local_subdirs(path):
     if is_collection(path):
         path = get_collection_path(path)
 
     return [item.name for item in path.iterdir() if item.is_dir()]
 
 
-def get_subdir_count(path):
+def get_local_subdir_count(path):
     if is_collection(path):
         path = get_collection_path(path)
-    
+
     return len([item for item in path.iterdir() if item.is_dir()])
 
 
-def list_subdirs(path):
+def list_local_subdirs(path):
     if is_collection(path):
         path = get_collection_path(path)
-    
+
     [print(item.name) for item in path.iterdir() if item.is_dir()]
     return None
 
@@ -219,13 +327,10 @@ def get_nested_counts(path):
     return files, dirs
 
 
-def list_keys(path_dict):
-    [print(key) for keys in path_dict]
-
 def create_subdir(dirname, dirlocation):
 
     # combine the path parts to get a single path
-    if is_string(dirname) and is_string(dirlocation):  
+    if is_string(dirname) and is_string(dirlocation):
       directory = combine_paths(dirlocation, dirname)
 
     # create directory object
@@ -243,7 +348,7 @@ def combine_paths(path):
 
 def create_dir(name):
     name = Path(Path.cwd() / name)
-    return name.mkdir(exist_ok=True) 
+    return name.mkdir(exist_ok=True)
 
 
 def is_path(obj):
@@ -254,8 +359,8 @@ def is_string(obj):
     return isinstance(obj, str)
 
 
-def is_dict(path):
-    return isinstance(path, dict)
+def is_collection(path):
+    return isinstance(path, namedtuple)
 
 
 def display_msg(error_msg):
@@ -277,18 +382,7 @@ def get_nested_files(path):
 
 def create_dir(name):
     name = Path(Path.cwd() / name)
-    return name.mkdir(exist_ok=True)  
-
-def set_ptype(path):
-    if is_path(path):
-        if is_dir(path):
-            ptype = 'directory'
-        else:
-            ptype = 'file'
-        return ptype
-    else:
-        display_error(ERROR_NOT_PATH_OBJECT)
-        return None
+    return name.mkdir(exist_ok=True)
 
 
 def update_path(path):
@@ -331,40 +425,32 @@ def list_all_contents(path):
         return None
 
 
-def show_path_info(path):
-    print(f"""Information:            {path['path']}
-    Path exists:                      {path['exists']}
-    Path type:                        {path['ptype']}
-    Name:                             {path['name']}
-    File type:                        {path['suffix']} 
-    Directory parts:                  {path['parts']}
-    Local file count:                 {path['total_local_files']}
-    Local directory count:            {path['total_local_dirs']}
-    Total file counts:                {path['total_files']}
-    Total directory counts:           {path['total_subdirs']} 
+def show_path_info(collection):
+    print(f"""Information:            {path.path}
+    Path exists:                      {path.exists}
+    Path type:                        {path.ptype}
+    Name:                             {path.name}
+    File type:                        {path.suffix}
+    Directory parts:                  {path.parts}
+    Local file count:                 {path.total_local_files}
+    Local directory count:            {path.total_local_dirs}
+    Total file counts:                {path.total_files}
+    Total directory counts:           {path.total_subdirs}
     """)
-   
-    if path['ptype'] == 'directory':
+
+    if collection.ptype == 'directory':
 
         print("\n\t\t\t\tLocal Folders: \n")
-        list_subdirs(path)
-        
+        collection.get_local_subdirs(collection.path)
+
         print("\n\t\t\t\tLocal Files: \n")
-        list_files(path)
-        
+        collection.get_local_files(collection.path)
+
         print("\n\t\t\t\tAll Files: \n")
-        list_all_files(path)
-        
+        collection.get_nested_files(collection.path)
+
         print("\n\t\t\t\tAll Folders: \n")
-        list_nested_subdirs(path)
-        
+        collection.get_nested_subdirs(collection.path)
+
     print("\n\t\t\t\t Directory Tree: \n")
-    display_directory_tree(path)
-
-
-def get_helper_path(helper):
-    return helper['path']
-
-
-def get_helper_exists(helper):
-    return helper['exists']
+    collection.display_directory_tree(collection.path)
