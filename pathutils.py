f4cdedb46b80b6a5f22198cb80e8182f017103d6
mmeastman1229 <matthew.m.eastman@gmail.com>
2023-11-11 00:13:29 -0800

    Updated Entire Script
    This is version 2 of the script. There are many changes to the functions
    and to the implementation. Using named tuple. All functions updated.

--- pathutils.py
+++ pathutils.py
@@ -43,91 +43,113 @@ SEARCH_PATTERN = '*'
 COLLECTION_TYPE = 'namedtuple'
 
 
+def is_path(obj):
+    return isinstance(obj, Path)
+
+
+def is_string(obj):
+    return isinstance(obj, str)
+
+
+def is_collection(collection, collection_type):
+    return isinstance(collection, collection_type)
+
+
+def is_phelper(path):
+    return isinstance(path, namedtuple)
+
+
+def path_exists(path):
+    return path.exists
+
+
+def is_dir(path):
+    return path.is_dir
+
+
+def is_file(path):
+    return path.is_file
+
+
 PHelper = namedtuple('PHelper', [
-            'self'
-            ,'exists'
-            ,'is_dir'
-            ,'is_file'
-            ,'parts'
-            ,'drive'
-            ,'root'
-            ,'anchor'
-            ,'parents'
-            ,'parent'
-            ,'name'
-            ,'suffix'
-            ,'suffixes'
-            ,'stem'
-            ,'cwd'
-            ,'home'
-            ,'stats'
-            ,'link_stats'
-            ,'as_posix'
-            ,'as_uri'
-            ,'get_owner'
-            ,'expanduser'
-            ,'is_absolute'
-            ,'is_reserved'
-            ,'is_block_device'
-            ,'is_char_device'
-            ,'is_fifo'
-            ,'is_mount'
-            ,'is_socket'
-            ,'is_symlink'
-            ,'get_linkk'
-            ,'hardlink_to'
-            ,'symlink_to'
-            ,'unlink'
-            ,'resolve'
-            ,'joinpath'
-            ,'match'
-            ,'iterdir'
-            ,'glob'
-            ,'rglob'
-            ,'group'
-            ,'change_name'
-            ,'change_name_and_type'
-            ,'change_type'
-            ,'make_absolute'
-            ,'change_mode'
-            ,'change_link_mode'
-            ,'mkdir'
-            ,'rmdir'
-            ,'open'
-            ,'touch'
-            ,'write_bytes'
-            ,'write_text'
-            ,'read_bytes'
-            ,'read_text'
-            ,'relative_to'
-            ,'rename'
-            ,'replace'
-            ,'samefile'
-            ,'ptype'
-            ,'info'
-            ,'display_tree'
-            ,'dir_contents'
-            ,'subdirs'
-            ,'all_contents'
-            ,'local_dirs'
-            ,'local_files'
-            ,'nested_dirs'
-            ,'nested_files'
-            ,'lfile_total'
-            ,'nfile_total'
-            ,'ndir_total'
-            ,'ldir_total'
+            'path'
+            , 'exists'
+            , 'is_dir'
+            , 'is_file'
+            , 'parts'
+            , 'drive'
+            , 'root'
+            , 'anchor'
+            , 'parents'
+            , 'parent'
+            , 'name'
+            , 'suffix'
+            , 'suffixes'
+            , 'stem'
+            , 'cwd'
+            , 'home'
+            , 'stats'
+            , 'link_stats'
+            , 'as_posix'
+            , 'as_uri'
+            , 'get_owner'
+            , 'expanduser'
+            , 'is_absolute'
+            , 'is_reserved'
+            , 'is_block_device'
+            , 'is_char_device'
+            , 'is_fifo'
+            , 'is_mount'
+            , 'is_socket'
+            , 'is_symlink'
+            , 'get_linkk'
+            , 'hardlink_to'
+            , 'symlink_to'
+            , 'unlink'
+            , 'resolve'
+            , 'joinpath'
+            , 'match'
+            , 'iterdir'
+            , 'glob'
+            , 'rglob'
+            , 'group'
+            , 'change_name'
+            , 'change_name_and_type'
+            , 'change_type'
+            , 'make_absolute'
+            , 'change_mode'
+            , 'change_link_mode'
+            , 'mkdir'
+            , 'rmdir'
+            , 'open'
+            , 'touch'
+            , 'write_bytes'
+            , 'write_text'
+            , 'read_bytes'
+            , 'read_text'
+            , 'relative_to'
+            , 'rename'
+            , 'replace'
+            , 'samefile'
+            , 'ptype'
+            , 'info'
+            , 'display_tree'
+            , 'tree_items'
+            , 'root_dirs'
+            , 'root_files'
+            , 'subdirs'
+            , 'subdir_files'
+            , 'rootfile_cnt'
+            , 'rootdir_cnt'
+            , 'subdir_cnt'
+            , 'subdir_file_cnt'
         ])
-
-
-def create_path(directory):
-    return Path(directory)
 
 
 def create_helper(path):
 
     path = PHelper(
-          self=path
+         path=path
         , exists=path.exists()
         , is_dir=path.is_dir()
         , is_file=path.is_file()
@@ -150,7 +172,7 @@ def create_helper(path):
         , get_owner=path.owner
         , expanduser=path.expanduser
         , is_absolute=path.is_absolute()
-        , is_reserved=path.is_reserved`
+        , is_reserved=path.is_reserved
         , is_block_device=path.is_block_device()
         , is_char_device=path.is_char_device()
         , is_fifo=path.is_fifo()
@@ -186,174 +208,72 @@ def create_helper(path):
         , rename=path.rename
         , replace=path.replace
         , samefile=path.samefile
-
-        # my custom functions
-        , ptype=get_ptype(path)
+        , ptype=_get_ptype(path)
         , info=show_path_info
-        , display_tree=display_directory_tree
-        , dir_contents=get_all_contents(path)
-        , subdirs=get_subdirs
-        , all_contents=get_all_contents(path)
-        , local_dirs=get_ldirs(path)
-        , local_files=get_lfiles(path)
-        , nested_dirs=get_ndirs(path)
-        , nested_files=get_nfiles(path)
-        , lfile_total=get_lfile_total(path)
-        , nfile_total=get_nfiles_total(path)
-        , ndir_total=get_ndir_total(path)
-        , ldir_total=get_ldir_total(path)
+        , display_tree=display_tree
+        , tree_items=_get_tree_items
+        , root_dirs=_get_root_dirs
+        , root_files=_get_root_files
+        , subdirs=_get_subdirs
+        , subdir_files=_get_subdir_files
+        , rootfile_cnt=_get_rootfiles_cnt
+        , rootdir_cnt=_get_rootdirs_cnt
+        , subdir_cnt=_get_subdirs_cnt
+        , subdir_file_cnt=_get_subdirfiles_cnt
     )
+
     return path
-
-def is_path(obj):
-    return isinstance(obj, Path)
-
-
-def is_string(obj):
-    return isinstance(obj, str)
-
-
-def is_collection(path, collection_type):
-    return isinstance(path, collection_type)
 
 
 def get_nondunder_fields(collection):
     return [field for field in dir(collection) if not field.startswith('__')]
 
 
+
 def get_all_fields(collection):
     return [field for field in collection._fields]
 
-
-def get_collection_type_path(collection):
-    return collection.path if is_collection(collection)
 
 
 def get_obj_type(obj):
     return type(obj)
 
 
-# local file functions
-def get_lfiles(path):
-    if is_collection(path):
-        path = get_collection_type_path(path)
 
-    return ([item.name for item in path.iterdir() if item.is_file()])
+def get_path(collection):
+    return collection.path
 
 
-def get_ldirs(path):
-    if is_collection(path):
-        path = get_collection_type_path(path)
 
-    return [item.name for item in path.iterdir() if item.is_dir()]
+def get_parts(path):
+    return path.parts
 
 
-def get_lfile_total(path):
-    if is_collection(path):
-        path = get_collection_type_path(path)
 
-    return len([item for item in path.iterdir() if item.is_file()])
+def get_subdir_cnt(path):
+    return len(path.nested_dirs)
 
 
-def list_local_files(path):
-    if is_collection(path):
-        path = get_collection_type_path(path)
 
-    [print(item.name) for item in path.iterdir() if item.is_file()]
-    return None
+def create_path(directory):
+    return Path(directory)
 
 
-# local folders
+
+def create_dir(name):
+    name = Path(Path.cwd() / name)
+    return name.mkdir(exist_ok=True)
 
 
-def get_ldir_total(path):
-    if is_collection(path):
-        path = get_collection_type_path(path)
+def create_dir(dirname, dirlocation):
 
-    return len([item for item in path.iterdir() if item.is_dir()])
-
-
-def list_local_subdirs(path):
-    if is_collection(path):
-        path = get_collection_type_path(path)
-
-    [print(item.name) for item in path.iterdir() if item.is_dir()]
-    return None
-
-
-# nested file functions
-def get_nfiles(path):
-    if is_collection(path):
-        path = get_collection_type_path(path)
-
-    return [item.name for item in path.rglob(SEARCH_PATTERN) if item.is_file()]
-
-
-def get_nfiles_total(path):
-    if is_collection(path):
-        path = get_collection_type_path(path)
-
-    return len([item for item in path.rglob(SEARCH_PATTERN) if item.is_file()])
-
-
-def list_all_files(path):
-    if is_collection(path):
-        path = get_collection_type_path(path)
-
-    [print(item.name) for item in path.rglob(SEARCH_PATTERN) if item.is_file()]
-    return None
-
-
-# Nested Subdirs
-def get_ndirs(path):
-    if is_collection(path):
-        path = get_collection_type_path(path)
-
-    return [item.name for item in path.rglob(SEARCH_PATTERN) if item.is_dir()]
-
-
-def get_ndirs_total(path):
-    if is_collection(path):
-        path = get_collection_type_path(path)
-
-    return len([item for item in path.rglob(SEARCH_PATTERN) if item.is_dir()])
-
-
-def list_nested_subdirs(path):
-    if is_collection(path):
-        path = get_collection_type_path(path)
-
-    [print(item.name) for item in path.rglob(SEARCH_PATTERN) if item.is_dir()]
-    return None
-
-
-def get_local_totals(path):
-    files = 0
-    dirs = 0
-    for item in path.iterdir():
-        if path.is_dir():
-            dirs += 1
-        else:
-            files += 1
-    return files, dirs
-
-
-def get_nested_totals(path):
-    files = 0
-    dirs = 0
-    for item in path.rglob(SEARCH_PATTERN):
-        if path.is_dir():
-            dirs += 1
-        else:
-            files += 1
-    return files, dirs
-
-
-def create_subdir(dirname, dirlocation):
+    if not is_path(dirname) and dirlocation is None:
+        display_error(ERROR_NOT_PATH_OBJECT)
+        return None
 
     # combine the path parts to get a single path
     if is_string(dirname) and is_string(dirlocation):
-      directory = combine_paths(dirlocation, dirname)
+        directory = combine_paths(dirlocation, dirname)
 
     # create directory object
     directory = create_path(directory)
@@ -364,58 +284,71 @@ def create_subdir(dirname, dirlocation):
     return directory
 
 
-def combine_paths(path):
-    Path.join(path)
-
-
-def create_dir(name):
-    name = Path(Path.cwd() / name)
-    return name.mkdir(exist_ok=True)
+def _get_ptype(collection):
+    return 'directory' if collection.is_dir() else 'file'
 
 
 
-
-def display_msg(error_msg):
-    print(error_msg)
-
-
-def get_path_parts(path):
-    return path.parts
-
-
-def get_nested_dirs(path):
-    return [item.name for item in path.rglob(SEARCH_PATTERN) if item.is_dir()]
-
-
-def get_nfiles(path):
-    return [item.name for item in path.rglob(SEARCH_PATTERN) if item.is_file()]
+def _get_root_dirs(path):
+    return list([item for item in path.iterdir() if item.is_dir()])
 
 
 
-def create_dir(name):
-    name = Path(Path.cwd() / name)
-    return name.mkdir(exist_ok=True)
+def _get_root_files(path):
+    return list([item for item in path.iterdir() if item.is_file()])
 
 
-def update_path(path):
-    if is_path(path['path']):
-        path['loc_files'] = get_lfiles(path['path'])
-        path['loc_dirs'] = get_local_directories(path['path'])
-        path['does_exist'] = path_exists(path['path'])
+
+def _get_subdirs(path):
+    return list([item for item in path.rglob(SEARCH_PATTERN) if item.is_dir()])
+
+
+
+def _get_subdir_files(path):
+    return list([item for item in path.rglob(SEARCH_PATTERN) if item.is_file()])
+
+
+
+def _get_tree_items(path):
+    return list([item for item in path.rglob(SEARCH_PATTERN)])
+
+
+def _get_rootfiles_cnt(path):
+    return len([item for item in path.iterdir() if item.is_file()])
+
+
+def _get_rootdirs_cnt(path):
+    return len([item for item in path.iterdir() if item.is_dir()])
+
+
+def _get_subdirs_cnt(path):
+    return len([item for item in path.rglob(SEARCH_PATTERN) if item.is_dir()])
+
+
+def _get_subdirfiles_cnt(path):
+    return len([item for item in path.rglob(SEARCH_PATTERN) if item.is_file()])
+
+
+
+def _get_treeitems_cnt(path):
+    return len([item for item in path.glob(SEARCH_PATTERN)])
+
+
+def create_folder_structure(path):
+    for folder_number, folder in enumerate(path.parents):
+        print(f'folder-{folder_number} is {folder}' )
+
+
+def display_msg(msg):
+    print(msg)
+
+
+
+def display_tree(directory):
+    if directory.is_dir:
+        path = directory
     else:
-        display_error(ERROR_NOT_PATH_OBJECT)
-        return None
-
-
-def display_directory_tree(directory):
-    if is_path(directory):
-        if is_dir(directory):
-            path = directory
-        else:
-            path = directory.parent
-    else:
-        display_error(ERROR_NOT_PATH_OBJECT)
-        return None
+        path = directory.parent
 
     print(f'+ {path}')
     for path_item in sorted(path.rglob(SEARCH_PATTERN)):
@@ -424,45 +357,151 @@ def display_directory_tree(directory):
         print(f'{spacer}+ {path_item.name}')
 
 
-def display_contents(contents):
-    for item in sorted(contents):
-        print(item)
+def list_subdir_files(phelper):
+   for file in phelper.subdir_files(phelper.path):
+       print(file.name)
 
 
-def list_all_contents(path):
-    if is_path(path):
-        [print(item.name) for item in path.rglob(SEARCH_PATTERN)]
-    else:
-        display_error(ERROR_NOT_PATH_OBJECT)
-        return None
+def list_subdirs(phelper):
+    for directory in phelper.subdirs(phelper.path):
+        print(directory.name)
 
 
-def show_path_info(collection):
-    print(f"""Information:            {collection.path}
-    Path exists:                      {collection.exists}
-    Path type:                        {collection.ptype}
-    Name:                             {collection.name}
-    File type:                        {collection.suffix}
-    Directory parts:                  {collection.parts}
-    Local file count:                 {collection.lfile_total}
-    Local directory count:            {collection.total_local_dirs}
-    Total file counts:                {collection.total_files}
-    Total directory counts:           {collection.ldir_total}
+def list_all_contents(phelper):
+    for item in phelper.tree_items(phelper.path):
+        print(item.name)
+
+
+def list_root_files(phelper):
+    for file in phelper.root_files(phelper.path):
+        print(file.name)
+
+
+def list_root_dirs(phelper):
+    for directory in phelper.root_dirs(phelper.path):
+        print(directory.name)
+
+
+def show_path_info(phelper):
+    print(f"""Information:      {phelper.path}
+    Path exists:                {phelper.exists}
+    Path type:                  {phelper.ptype}
+    Name:                       {phelper.name}
+    File type:                  {phelper.suffix}
+    Directory parts:            {phelper.parts}
+    Local file count:           {phelper.rootfile_cnt(phelper.path)}
+    Local directory count:      {phelper.rootdir_cnt(phelper.path)}
+    Total file counts:          {phelper.subdir_file_cnt(phelper.path)}
+    Total directory counts:     {phelper.subdir_cnt(phelper.path)}
     """)
 
-    if collection.ptype == 'directory':
+    if phelper.ptype == 'directory':
 
         print("\n\t\t\t\tLocal Folders: \n")
-        collection.get_ldirs(collection.path)
+        list_root_dirs(phelper)
 
         print("\n\t\t\t\tLocal Files: \n")
-        collection.get_lfiles(collection.path)
+        list_root_files(phelper)
 
         print("\n\t\t\t\tAll Files: \n")
-        collection.get_nfiles(collection.path)
+        list_subdir_files(phelper)
 
         print("\n\t\t\t\tAll Folders: \n")
-        collection.get_ndirs(collection.path)
+        list_subdirs(phelper)
 
-    print("\n\t\t\t\t Directory Tree: \n")
-    collection.display_directory_tree(collection.path)
+    print("\n\t\t\t Directory Tree: \n")
+    phelper.display_tree(phelper.path)
+
+
+def bulk_name_change(phelper, old_str, new_str):
+    directory = phelper.path
+    pattern = f'*{old_str}*'
+    matching_files = directory.rglob(pattern)
+
+    for path in matching_files:
+        new_name = path.name.replace(old_str, new_str)
+        path.rename(path.with_name(new_name))
+
+
+def find_duplicates(lst):
+    unique_set = set()
+    duplicates = set()
+    dup_info = {}
+
+    for item in lst:
+        if item in unique_set:
+            duplicate.add(item)
+        else:
+            unique_set.add(item)
+
+    return duplicates
+
+
+def get_file_types(path):
+    file_types = set()
+    for file in path:
+        file_types.add(file.suffix)
+    return file_types
+
+
+def file_check(file1, file2):
+    return cmp(file1, file2)
+
+
+def move_or_rename(fname, source, destination):
+
+    # check if source and destination are strings
+
+    if is_string(source):
+        source = create_path(source)
+
+    if is_string(destination):
+        destination = create_path(destination)
+
+    source = source / fname
+    destination = destination / fname
+
+    if not destination.exists():
+        source.replace(destination)
+
+
+def move_files(source_folder, destination_folder, filetype):
+    search_str = f'*{filetype}*'
+    for file in source_folder.rglob(search_str):
+        source_path = Path(file)
+        destination_path = destination_folder / file.name
+        source_path.rename(destination_path)
+
+
+def move_all_txt(source_folder, destination_folder):
+    if is_string(source_folder):
+        source_folder = create_path(source_folder)
+
+    if is_string(destination_folder):
+        destination_folder = create_path(destination_folder)
+
+    for file in source_folder.glob('*'):
+        if is_file(file) and file.suffix == '.txt':
+            move_or_rename(file.name, source_folder, destination_folder)
+        else:
+            continue
+
+
+def move_all_file_types(ftype, files, destination):
+
+    # Set the destination path
+    destination = create_path(destination)
+
+
+    # get files by passing helper function
+    if is_helper(files):
+
+        files = files['all_dir_sub_files']
+        for file in files:
+
+            # Define the old source and new source
+            file_src = file.parent / file.namev
+            file_dst = destination / file.name
+
+            if not file_dst.exists() and file.suffix == ftype:
+                move_or_rename(file.name, file_srce, file_dst)
